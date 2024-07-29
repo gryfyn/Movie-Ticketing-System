b2e9a1da8e0e2fd65903ac9aa721f48702ef5824
Your Name <your.email@example.com>
2024-07-29 21:33:09 +0300

    first commit
    all work

---
+++ src/App.test.js
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/home/HomePage';
+import MovieDetails from './components/browse/MovieDetails';
+import MoviePage from './components/browse/MoviePage';
+import BookingPage from './components/booking/BookingPage';
+import ReceiptPage from './components/receipts/ReceiptPage';
+import PaymentPage from './components/payment/PaymentPage';
+
+test('Renders HomePage component for "/" route', async () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </MemoryRouter>
+  );
+  const homeElement = await screen.findByTestId('homepage'); 
+  expect(homeElement).toBeInTheDocument();
+});
+
+test('Renders MovieDetails component for "/movie/:id" route', async () => {
+  render(
+    <MemoryRouter initialEntries={['/movie/1']}>
+      <Routes>
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
+    </MemoryRouter>
+  );
+  const movieDetailsElement = await screen.findByText(/PG/);
+  expect(movieDetailsElement).toBeInTheDocument();
+});
+
+test('Renders MoviePage component for "/movies" route', async () => {
+  render(
+    <MemoryRouter initialEntries={['/movies']}>
+      <Routes>
+        <Route path="/movies" element={<MoviePage />} />
+      </Routes>
+    </MemoryRouter>
+  );
+  const moviePageElement = await screen.findByText(/Movies/i); // Replace with actual text
+  expect(moviePageElement).toBeInTheDocument();
+});
+
